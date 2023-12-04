ab43ae03383e039cfecc4af4e64e1a9615927496
Sajid Salhi <sajid.s35400@gmail.com>
2023-12-04 12:08:02 +0100

    ok

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,34 +7,45 @@ use Symfony\Component\HttpClient\Response\ResponseStream;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProgramRepository;
+use App\Repository\CategoryRepository;
 
-#[Route('/category/', name: 'category_index')]
+#[Route('/category', name: 'category_')]
 
 class CategoryController extends AbstractController
 {
-    #[Route('/category/', name: 'category_index')]
-    public function index(ProgramRepository $programRepository): Response
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        $category = $categoryRepository->findBy();
+        $categories = $categoryRepository->findAll();
 
         return $this->render(
-            'category/category_index.html.twig',
-             ['category' => $category]);
+            'category/index.html.twig',
+             ['categories' => $categories]);
     }
+ 
+ 
+    #[Route('/show/{categoryName}', name: 'show')]
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
+    {
+        $categories = $categoryRepository->findOneBy(['name' => $categoryName]);
+        if (!$categories)
+        {
+            throw $this->createNotFoundException(
+                'No program with id : found in category\'s table.'
+            );
+        }
+        
+        $programs = $programRepository->findBy(['category' => $categories->getId()], ['id' => 'DESC'], 3);
+        if (!$programs)
+        {
+            throw $this->createNotFoundException(
+                'No program with id : found in program\'s table.'
+            );
+        }
 
-    #[Route('/category/{categoryName}', name: 'category_show')]
-public function show(int $id, CategoryRepository $categoryRepository):Response
-{
-    $category = $categoryRepository->findBy(['id' => $id]);
-    // same as $category = $categoryRepository->findBy($id);
-
-    if (!$category) {
-        throw $this->createNotFoundException(
-            'No category with id : '.$id.' found in category\'s table.'
-        );
+        return $this->render('category/show.html.twig', [
+            'programs' => $programs,
+            'category' => $categories,
+        ]);
     }
-    return $this->render('category/category_show.html.twig', [
-        'category' => $category,
-    ]);
-}
 }
