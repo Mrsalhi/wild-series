8e03267e8c38c34514b2fe5e98b7a56b37a77fe3
Sajid Salhi <sajid.s35400@gmail.com>
2023-12-04 17:18:41 +0100

    ok

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -15,22 +15,46 @@ $episode->setSeason($this->getReference('season1_Arcane'));
 //... create 2 more episodes
 //src/DataFixtures/EpisodeFixtures.php
 $episode = new Episode();
-$episode->setTitle('Certains mystères ne devraient jamais être résolus');
-$episode->setNumber(2);
+$episode->setTitle('Cette violence crasse nécessaire au changement');
+$episode->setNumber(3);
 $episode->setSeason($this->getReference('season1_Arcane'));
 //... set other episode's properties
 //... create 2 more episodes
 //src/DataFixtures/EpisodeFixtures.php
 $episode = new Episode();
-$episode->setTitle('Certains mystères ne devraient jamais être résolus');
-$episode->setNumber(2);
+$episode->setTitle('Joyeuse Fête du progrès !');
+$episode->setNumber(4);
 $episode->setSeason($this->getReference('season1_Arcane'));
 //... set other episode's properties
 //... create 2 more episodes
 //src/DataFixtures/EpisodeFixtures.php
 $episode = new Episode();
-$episode->setTitle('Certains mystères ne devraient jamais être résolus');
-$episode->setNumber(2);
+$episode->setTitle('L ennemi commun');
+$episode->setNumber(5);
+$episode->setSeason($this->getReference('season1_Arcane'));
+//... set other episode's properties
+//... create 2 more episodes
+$episode = new Episode();
+$episode->setTitle('Quand l empire s effondre');
+$episode->setNumber(6);
+$episode->setSeason($this->getReference('season1_Arcane'));
+//... set other episode's properties
+//... create 2 more episodes
+$episode = new Episode();
+$episode->setTitle('Le petit sauveur');
+$episode->setNumber(7);
+$episode->setSeason($this->getReference('season1_Arcane'));
+//... set other episode's properties
+//... create 2 more episodes
+$episode = new Episode();
+$episode->setTitle('L eau et l huile');
+$episode->setNumber(8);
+$episode->setSeason($this->getReference('season1_Arcane'));
+//... set other episode's properties
+//... create 2 more episodes
+$episode = new Episode();
+$episode->setTitle('Rouages et engrenages');
+$episode->setNumber(9);
 $episode->setSeason($this->getReference('season1_Arcane'));
 //... set other episode's properties
 //... create 2 more episodes
