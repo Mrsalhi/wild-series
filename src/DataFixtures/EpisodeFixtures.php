ebcd03f6dfcf1915ce2ced7b58d65cec994ee802
Sajid Salhi <sajid.s35400@gmail.com>
2023-12-05 10:49:47 +0100

    ko

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -1,60 +1,66 @@
 <?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Episode;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
 //src/DataFixtures/EpisodeFixtures.php
 $episode = new Episode();
 $episode->setTitle('Welcome to the Playground');
 $episode->setNumber(1);
 $episode->setSeason($this->getReference('season1_Arcane'));
-//... set other episode's properties
-//... create 2 more episodes
-//src/DataFixtures/EpisodeFixtures.php
+$episode->setSynopsis('Les sœurs orphelines Vi et Powder causent des remous dans les rues souterraines de Zaun à la suite d un braquage dans le très huppé Piltover. ');
+
 $episode = new Episode();
 $episode->setTitle('Certains mystères ne devraient jamais être résolus');
 $episode->setNumber(2);
 $episode->setSeason($this->getReference('season1_Arcane'));
-//... set other episode's properties
-//... create 2 more episodes
-//src/DataFixtures/EpisodeFixtures.php
+$episode->setSynopsis('Idéaliste, le chercheur Jayce tente de maîtriser la magie par la science malgré les avertissements de son mentor. Le criminel Silco teste une substance puissante.');
+
 $episode = new Episode();
 $episode->setTitle('Cette violence crasse nécessaire au changement');
 $episode->setNumber(3);
 $episode->setSeason($this->getReference('season1_Arcane'));
-//... set other episode's properties
-//... create 2 more episodes
-//src/DataFixtures/EpisodeFixtures.php
+$episode->setSynopsis('Deux anciens rivaux s affrontent lors d un défi spectaculaire qui se révèle fatidique pour Zaun. Jayce et Viktor prennent de gros risques pour leurs recherches.'); 
+
 $episode = new Episode();
 $episode->setTitle('Joyeuse Fête du progrès !');
 $episode->setNumber(4);
 $episode->setSeason($this->getReference('season1_Arcane'));
-//... set other episode's properties
-//... create 2 more episodes
-//src/DataFixtures/EpisodeFixtures.php
+$episode->setSynopsis('Alors que Piltover profite de leur technologie, Jayce et Viktor réfléchissent à ce qu ils vont faire. Un visage familier ressort de Zaun pour semer la pagaille.');
+
+
 $episode = new Episode();
 $episode->setTitle('L ennemi commun');
 $episode->setNumber(5);
 $episode->setSeason($this->getReference('season1_Arcane'));
-//... set other episode's properties
-//... create 2 more episodes
+$episode->setSynopsis('Caitlyn, la pacifieuse qui n en fait qu à sa tête, arpente les bas-fonds pour trouver Silco. Jayce devient une cible en combattant la corruption à Piltover.');
+
+
 $episode = new Episode();
 $episode->setTitle('Quand l empire s effondre');
 $episode->setNumber(6);
 $episode->setSeason($this->getReference('season1_Arcane'));
-//... set other episode's properties
-//... create 2 more episodes
+$episode->setSynopsis('Un protégé discrédite son mentor devant le Conseil à cause de l évolution rapide d une technologie magique. Poursuivie par les autorités, Jinx doit affronter son passé.');
+
 $episode = new Episode();
 $episode->setTitle('Le petit sauveur');
 $episode->setNumber(7);
 $episode->setSeason($this->getReference('season1_Arcane'));
-//... set other episode's properties
-//... create 2 more episodes
+$episode->setSynopsis('Caitlyn et Vi retrouvent un allié dans les rues de Zaun et se déchaînent contre une ennemie commune. Viktor prend une décision désespérée.');
+
+
 $episode = new Episode();
 $episode->setTitle('L eau et l huile');
 $episode->setNumber(8);
 $episode->setSeason($this->getReference('season1_Arcane'));
-//... set other episode's properties
-//... create 2 more episodes
+$episode->setSynopsis('L héritière désavouée Mel et sa mère échangent des tactiques de combat. Caitlyn et Vi forgent une alliance improbable. Jinx subit une étonnante transformation.');
+
+
 $episode = new Episode();
 $episode->setTitle('Rouages et engrenages');
 $episode->setNumber(9);
 $episode->setSeason($this->getReference('season1_Arcane'));
-//... set other episode's properties
-//... create 2 more episodes
+$episode->setSynopsis('À deux doigts d entrer en guerre, les chefs de Piltover et de Zaun sont face à un ultimatum, jusqu à ce qu un affrontement fatidique bouleverse les deux villes à jamais.');
--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -1,7 +1,251 @@
 <?php
-//src/DataFixtures/SeasonFixtures.php
+namespace App\DataFixtures;
+
+use App\Entity\Season;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+
+
+
 $season = new Season();
 $season->setNumber(1);
 $season->setProgram($this->getReference('program_Arcane'));
-//... set other season's properties
+
 $this->addReference('season1_Arcane', $season);
+
+
+//src/DataFixtures/SeasonFixtures.php
+$season = new Season();
+$season->setNumber(1);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season1_Walking dead', $season);
+
+
+//src/DataFixtures/SeasonFixtures.php
+$season = new Season();
+$season->setNumber(2);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season2_Walking dead', $season);
+
+
+//src/DataFixtures/SeasonFixtures.php
+$season = new Season();
+$season->setNumber(3);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season3_Walking dead', $season);
+
+//src/DataFixtures/SeasonFixtures.php
+$season = new Season();
+$season->setNumber(4);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season4_Walking dead', $season);
+
+$season = new Season();
+$season->setNumber(5);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season5_Walking dead', $season);
+
+
+$season = new Season();
+$season->setNumber(6);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season6_Walking dead', $season);
+
+
+$season = new Season();
+$season->setNumber(7);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season7_Walking dead', $season);
+
+
+$season = new Season();
+$season->setNumber(8);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season8_Walking dead', $season);
+
+
+
+$season = new Season();
+$season->setNumber(9);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season9_Walking dead', $season);
+
+
+$season = new Season();
+$season->setNumber(10);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season10_Walking dead', $season);
+
+
+$season = new Season();
+$season->setNumber(11);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season11_Walking dead', $season);
+
+
+
+$season = new Season();
+$season->setNumber(1);
+$season->setProgram($this->getReference('program_Walking dead'));
+//... set other season's properties
+$this->addReference('season1_Walking dead', $season);
+
+
+$season = new Season();
+$season->setNumber(1);
+$season->setProgram($this->getReference('program_Jujutsu Kaisen'));
+//... set other season's properties
+$this->addReference('season1_Jujutsu Kaisen', $season);
+
+
+$season = new Season();
+$season->setNumber(2);
+$season->setProgram($this->getReference('program_Jujutsu Kaisen'));
+//... set other season's properties
+$this->addReference('season2_Jujutsu Kaisen', $season);
+
+$season = new Season();
+$season->setNumber(1);
+$season->setProgram($this->getReference('program_Gen V'));
+//... set other season's properties
+$this->addReference('season1_Gen V', $season);
+
+$season = new Season();
+$season->setNumber(1);
+$season->setProgram($this->getReference('program_Good Omens'));
+//... set other season's properties
+$this->addReference('season1_Good Omens', $season);
+
+$season = new Season();
+$season->setNumber(2);
+$season->setProgram($this->getReference('program_Good Omens'));
+//... set other season's properties
+$this->addReference('season2_Good Omens', $season);
+
+
+$season = new Season();
+$season->setNumber(1);
+$season->setProgram($this->getReference('program_kaamelott'));
+//... set other season's properties
+$this->addReference('season1_kaamelott', $season);
+
+$season = new Season();
+$season->setNumber(2);
+$season->setProgram($this->getReference('program_kaamelott'));
+//... set other season's properties
+$this->addReference('season2_kaamelott', $season);
+
+$season = new Season();
+$season->setNumber(3);
+$season->setProgram($this->getReference('program_kaamelott'));
+//... set other season's properties
+$this->addReference('season3_kaamelott', $season);
+
+$season = new Season();
+$season->setNumber(4);
+$season->setProgram($this->getReference('program_kaamelott'));
+//... set other season's properties
+$this->addReference('season4_kaamelott', $season);
+
+
+$season = new Season();
+$season->setNumber(5);
+$season->setProgram($this->getReference('program_kaamelott'));
+//... set other season's properties
+$this->addReference('season5_kaamelott', $season);
+
+
+$season = new Season();
+$season->setNumber(6);
+$season->setProgram($this->getReference('program_kaamelott'));
+//... set other season's properties
+$this->addReference('season6_kaamelott', $season);
+
+
+$season = new Season();
+$season->setNumber(1);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season1_American Horror Story', $season);
+
+
+$season = new Season();
+$season->setNumber(2);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season2_American Horror Story', $season);
+
+
+$season = new Season();
+$season->setNumber(3);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season3_American Horror Story', $season);
+
+$season = new Season();
+$season->setNumber(4);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season4_American Horror Story', $season);
+
+$season = new Season();
+$season->setNumber(5);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season5_American Horror Story', $season);
+
+$season = new Season();
+$season->setNumber(6);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season6_American Horror Story', $season);
+
+$season = new Season();
+$season->setNumber(7);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season7_American Horror Story', $season);
+
+$season = new Season();
+$season->setNumber(8);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season8_American Horror Story', $season);
+
+$season = new Season();
+$season->setNumber(9);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season9_American Horror Story', $season);
+
+$season = new Season();
+$season->setNumber(10);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season10_American Horror Story', $season);
+
+
+$season = new Season();
+$season->setNumber(11);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season11_American Horror Story', $season);
+
+$season = new Season();
+$season->setNumber(12);
+$season->setProgram($this->getReference('program_American Horror Story'));
+//... set other season's properties
+$this->addReference('season12_American Horror Story', $season);
+
