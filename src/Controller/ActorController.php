247694e72ce294e7cb3d7b2d32b3c666d8a81da6
Sajid Salhi <sajid.s35400@gmail.com>
2023-12-18 11:58:43 +0100

    OK

---
+++ src/Controller/ActorController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controller;
+use App\Entity\Actor;
+use App\DataFixtures\ActorFixtures;
+use App\DataFixtures\ProgramFixtures;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ActorController extends AbstractController
+{
+    #[Route('/actor/{id}', name: 'actor_show')]
+    public function show(Actor $actor): Response
+    {
+            if (!$actor) {
+            throw $this->createNotFoundException(
+                  'no program with id : ' . $actor . 'found in program\'s table.'
+            );
+        }
+        return $this->render('actor/show.html.twig', ['actor' => $actor]);
+    }
+  
+
+} 
--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -1,29 +1,39 @@
 <?php
 
 namespace App\DataFixtures;
-
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Faker\Factory;
+use App\Entity\Actor;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use App\DataFixtures\actor;
 
-class ActorFixtures extends Fixture
+class ActorFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
         $faker = Factory::create();
+        $programReference = [];
+        for ($p = 1; $p <= 5; $p++) {
+            $programReference[] = $this->getReference('program_' . $p);
+        }
 
-        for($i = 0; $i < 10; $i++) {
-            $actor = new actor();
-           
-            $actor->setNumber($faker->numberBetween(1, 10));
-            $actor->setname($faker->name());
-            $actor->setprogram($faker->program(3, true));
-       
+        for ($i = 0; $i < 10; $i++) {
+            $actor = new Actor();
+            $actor->setName($faker->name);
+            shuffle($programReference);
+            for ($a = 0; $a < 3; $a++) {
+                $actor->addProgram($programReference[$a]);
+            }
             $manager->persist($actor);
         }
+
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            ProgramFixtures::class,
+        ];
     }
 }
