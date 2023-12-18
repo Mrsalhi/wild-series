289542722d4e346bc9f34fdf55a8bc2d8bef2710
Sajid Salhi <sajid.s35400@gmail.com>
2023-12-18 09:51:39 +0100

    ok

---
+++ src/DataFixtures/ActorFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+;
+
+class ActorFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $faker = Factory::create();
+
+        for($i = 0; $i < 10; $i++) {
+            $actor = new actor();
+           
+            $actor->setNumber($faker->numberBetween(1, 10));
+            $actor->setname($faker->name());
+            $actor->setprogram($faker->program(3, true));
+       
+            $manager->persist($actor);
+        }
+        $manager->flush();
+    }
+}
